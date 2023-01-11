f02b166dabbadb5c3fa1f723736598476f60fec7
jason34567 <121881574+jason34567@users.noreply.github.com>
2023-01-11 11:46:41 -0500

    Add files via upload

---
+++ ex.20.js
@@ -0,0 +1,74 @@
+//Read https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
+// https://www.w3schools.com/js/js_string_templates.asp
+
+let student = {
+    name: "John Doe",
+    age: 24,
+    id: "123456789",
+    gender: "male",
+    address: {
+        number: "56",
+        street: "Main St.",
+        city: "Brooklyn",
+        zip: "12345",
+        state: "NY"
+    },
+};
+
+document.getElementById('txtName').innerHTML = student.name;
+document.getElementById("txtID").innerHTML = student.id;
+document.getElementById("txtCourses").innerHTML = 'Course';
+document.getElementById("txtAddress").innerHTML = student.address;
+document.getElementById("txtAge").innerHTML = student.age;
+
+
+let empList =[
+{
+    "fname": "Sharad",
+    "lname": "Volz"
+},
+{
+    "fname": "Constance",
+    "lname": "Dech"
+},
+{
+    "fname": "Myra",
+    "lname": "Comeau"
+},
+{
+    "fname": "Noriess",
+    "lname": "Dabbs"
+},
+{
+    "fname": "Esperanza",
+    "lname": "Shrestha"
+},
+{
+    "fname": "Eugene",
+    "lname": "Barron"
+},
+{
+    "fname": "Petr",
+    "lname": "Ravenelle"
+},
+{
+    "fname": "Cristina",
+    "lname": "Terry"
+},
+{
+    "fname": "Victoria",
+    "lname": "Aresti"
+},
+{
+    "fname": "Byron",
+    "lname": "Yarber"
+},
+];
+
+
+
+
+
+
+
+
